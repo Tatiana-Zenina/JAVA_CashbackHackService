142ba57a33a96631daddb95a93fb0b54ef2c976a
HW Netology <zenina_t@inbox.ru>
2023-06-07 01:46:22 +0400

    CashbackHackService_JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,53 +4,53 @@ package ru.netology.service;
 public class CashbackHackServiceTest {
 
 
-    @org.testng.annotations.Test //№1
+    @org.junit.Test //№1
     public void shouldCalculateWhen500() {
         CashbackHackService service = new CashbackHackService();
         int amount = 500;
         int expected = 500;
         int actual = service.remain(amount);
 
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test //№2
+    @org.junit.Test //№2
     public void shouldCalculateWhen1753() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1753;
         int expected = 247;
         int actual = service.remain(amount);
 
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test //№3
+    @org.junit.Test //№3
     public void shouldCalculateWhen999() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
         int expected = 1;
         int actual = service.remain(amount);
 
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test //№4
+    @org.junit.Test //№4
     public void shouldCalculateWhen1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
 
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test //№5
+    @org.junit.Test //№5
     public void shouldCalculateWhen1001() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int expected = 999;
         int actual = service.remain(amount);
 
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 }
